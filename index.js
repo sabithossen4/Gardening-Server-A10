edf3890e100413619f0c1432268790ae2a9f7adc
sabithossen4 <sabithossenkhoka420@gmail.com>
2025-05-22 18:29:59 +0600

    added a delete

--- index.js
+++ index.js
@@ -69,6 +69,13 @@ async function run() {
         res.send(result);
       })
 
+      app.delete('/gardens/:id' ,async(req,res) =>{
+        const id = req.params.id;
+        const query ={_id: new ObjectId(id)}
+        const result = await gardensCollection.deleteOne(query);
+        res.send(result)
+      })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
